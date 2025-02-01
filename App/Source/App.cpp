abd4eeb472f67cd2ea64fe38b32f0cdcbf5301ad
luischavesdev <115810776+luischavesdev@users.noreply.github.com>
2025-02-01 18:42:15 +0000

    proto input management

--- App/Source/App.cpp
+++ App/Source/App.cpp
@@ -11,6 +11,7 @@ std::random_device randomGenerator;
 std::mt19937 randomEngine(randomGenerator());
 
 
+
 void RandomizePool()
 {
 	std::shuffle(pool.begin(), pool.end(), randomEngine);
@@ -36,9 +37,12 @@ public:
 	static const int columns = 5;
 	static const int size = rows * columns;
 
-	Card()
+	Card(const bool& populate = false)
 	{
-		PopulateCells();
+		if (populate)
+		{
+			PopulateCells();
+		}
 	}
 
 	void ReshuffleCells()
@@ -126,17 +130,37 @@ private:
 
 
 int main() {
-
 	Initialization();
 
-	Card crd = Card();
+	int credits = 1;
+	int maxCredits = 999;
+	int addedCredits = 0;
 
+	char input = '\0';
+
+	Card crd = Card();
 	Extractor ex = Extractor();
+
+
+	std::cout << "How many credits to start?\n";
+	std::cin >> addedCredits;
+	credits = std::max(0, std::min(credits + addedCredits, maxCredits));
+	std::cout << "Proceed to show card. (y)\n";
+	std::cin >> input;
+	
+	do
+	{
+		crd.ReshuffleCells();
+		std::cout << "Generate another card? (y/n)\n";
+		std::cin >> input;
+	} while (!std::cin.fail() && (input == 'y'));
+
+	
+	std::cout << "Draw balls. (y)\n";
+	std::cin >> input;
 	ex.Draw();
 
-	std::cin.get();
-
-	crd.CheckHits(ex.balls);
+	//crd.CheckHits(ex.balls);
 
 
 	std::cin.get();
